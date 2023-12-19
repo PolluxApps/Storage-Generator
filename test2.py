c1ad6c2cc8bb92d77f96434780db3f6f260fa851
Jakob <84283522+Mee632@users.noreply.github.com>
2023-12-19 14:03:02 +0100

    test 2

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 def video_to_bits(video_file, width, height, fps):
     cap = cv2.VideoCapture(video_file)
     bits = ''
@@ -18,16 +19,17 @@ def video_to_bits(video_file, width, height, fps):
     cap.release()
     return bits
 
+
 def bits_to_file(bits, output_file):
     with open(output_file, 'wb') as file:
         for i in range(0, len(bits), 8):
-            byte = bits[i:i+8]
+            byte = bits[i:i + 8]
             byte = int(byte, 2)
             file.write(byte.to_bytes(1, byteorder='big'))
 
 
 video_file = 'output_video.mp4'  # Pfad zum Video
-output_file = 'output_file.txt'         # Ausgabedatei
+output_file = 'output_file.txt'  # Ausgabedatei
 fps = 25  # Bildrate des Videos
 
 bits = video_to_bits(video_file, 1920, 1080, fps)
